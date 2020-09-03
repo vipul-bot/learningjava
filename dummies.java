1ab01776afb71aff9f9da2c8915a5ae58b71342e
vipul-mark1 <vdas111111@gmail.com>
2020-09-03 18:10:32 +0530

    Add files via upload

---
+++ dummies.java
@@ -0,0 +1,27 @@
+//to print Java for dummies using java
+import java.util.Scanner;
+class dummies{
+  
+static String text=" Java for dummies";
+         String text1= "Are you Dummy ?"; // are you dummy
+         String Answer;
+         // then why are you coding in java
+void code(){
+  System.out.println("Then Why are you Coding in Java");
+
+}
+public static void main(String [] args){
+ dummies dumbo=new dummies();
+ Scanner scan=new Scanner(System.in);
+ System.out.println(text);
+ System.out.println(dumbo.text1);
+ dumbo.code();
+ //yes
+dumbo.Answer=scan.next();
+System.out.println("Answer :"+dumbo.Answer);
+}
+}
+
+ 
+
+          
